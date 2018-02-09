1a3316cafec3a38046ac64c837a81a4e62989a78
Evita Kusinya <vita.kusinya@gmail.com>
2018-02-09 22:04:09 +0200

    add guessing game

---
+++ homework_07/js/task2.js
@@ -0,0 +1,74 @@
+let game = confirm("Do you want to play a game?");
+let maxPrize = 10;
+let totalPrize = 0;
+let minRandom = 0;
+let maxRandom = 5;
+
+if (game) {
+  while (game) {
+    let numRandom =
+      Math.floor(Math.random() * (maxRandom - minRandom + 1)) + minRandom;
+    for (let i = 0; i < 3; i++) {
+      let numUser = Number(
+        prompt(
+          "Enter a number from " +
+            minRandom +
+            " to " +
+            maxRandom +
+            "\nAttempts left: " +
+            (3 - i) +
+            "\nTotal prize: " +
+            totalPrize +
+            "$" +
+            "\nPossible prize on current attempt: " +
+            maxPrize +
+            "$"
+        )
+      );
+      if (numUser === numRandom) {
+        totalPrize += maxPrize;
+        alert(
+          "Thank you for a game. Your prize is: " +
+            maxPrize +
+            "$" +
+            "\nTotal prize: " +
+            totalPrize +
+            "$"
+        );
+        game = confirm("Do you want to continue a game?");
+        if (game) {
+          minRandom *= 2;
+          maxRandom *= 2;
+          maxPrize *= 3;
+          i = -1;
+          numRandom =
+            Math.floor(Math.random() * (maxRandom - minRandom + 1)) + minRandom;
+        } else {
+          break;
+        }
+      } else if (i == 2) {
+        game = confirm(
+          "Thank you for a game. Your prize is: " +
+            totalPrize +
+            "$" +
+            "\nDo you want to play again?"
+        );
+        if (game) {
+          totalPrize = 0;
+          maxPrize = 10;
+          minRandom = 0;
+          maxRandom = 5;
+          i = -1;
+          numRandom =
+            Math.floor(Math.random() * (maxRandom - minRandom + 1)) + minRandom;
+        } else {
+          break;
+        }
+      } else {
+        maxPrize = Math.floor(maxPrize / 2);
+      }
+    }
+  }
+} else {
+  alert("You did not became a millionaire");
+}
