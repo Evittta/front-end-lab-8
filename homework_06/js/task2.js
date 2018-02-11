daebf58f30b63ffb2d7247bbc4aee999e59b2234
Evita Kusinya <vita.kusinya@gmail.com>
2018-02-11 22:59:29 +0200

    add checking of input data

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,8 +1,14 @@
-const amountEuro = Number(prompt(`Please input amount of euro`, `10`));
-const amountUsd = Number(prompt('Please input amount of usd', `10`));
 const euro = 33.2324;
 const usd = 27.124;
+let amountEuro = Number(prompt(`Please input amount of euro`, `10`));
+let amountUsd = Number(prompt('Please input amount of usd', `10`));
 
-console.log(`${amountEuro} euro are equal ${parseInt(amountEuro * euro)} UAH, 
-${amountUsd} are equal ${parseInt(amountUsd * usd)} UAH, 
-one euro is equal ${parseFloat(euro / usd).toFixed(2)} dollars`);
+let euroToUah = parseFloat((amountEuro * euro).toFixed(2));
+let dollarToUah = parseFloat((amountUsd * usd).toFixed(2));
+let euroToDollar = parseFloat((euro / usd).toFixed(2));
+
+if(amountEuro < 0 || amountUsd < 0 || isNaN(amountEuro) || isNaN(amountUsd)) {
+    console.error(`Incorrect data`);
+} else {
+    console.log(`${amountEuro} euro are equal ${euroToUah} UAH, ${amountUsd} dollars are equal ${dollarToUah} UAH, one euro is equal ${euroToDollar} dollars`);
+}
