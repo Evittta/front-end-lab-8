c3b1265695924cfae3a29710cf7bd5cf36373b26
Evita Kusinya <vita.kusinya@gmail.com>
2018-02-13 01:02:18 +0200

    change repeat parts of code

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -1,58 +1,42 @@
 let game = confirm(`Do you want to play a game?`);
 let maxPrize = 10;
-let prize = 10;
 let totalPrize = 0;
 let maxRandom = 5;
+let win = false;
+let nextGame = true;
 
 if (game) {
-  let numRandom = Math.floor(Math.random() * (maxRandom + 1));
+  let numRandom = Math.floor(Math.random() * maxRandom + 1);
+  let prize = maxPrize;
   for (let i = 0; i < 3; i++) {
-    let numUser = Number(prompt(`Enter a number from 0 to ${maxRandom}\n` +
-                                `Attempts left: ${3 - i}\n` +
-                                `Total prize: ${totalPrize}$\n` +
-                                `Possible prize on current attempt: ${prize}$`));
-    if (numUser === numRandom) {
+    let numUser = prompt(`Enter a number from 0 to ${maxRandom}\n` +
+                         `Attempts left: ${3 - i}\n` +
+                         `Total prize: ${totalPrize}$\n` +
+                         `Possible prize on current attempt: ${prize}$`);
+    if ( numUser == numRandom && numUser != "" ) {
       totalPrize += prize;
-      game = confirm(`Do you want to continue a game?`);
-      if (game) {
-        maxRandom *= 2;
-        maxPrize *= 3;
-        prize = maxPrize;
-        i = -1;
-        numRandom = Math.floor(Math.random() * (maxRandom + 1));
-      } else {
-        game = confirm(
-          `Thank you for a game. Your prize is: ${totalPrize}$\n` +
-            `Do you want to play again?`
-        );
-        if (game) {
-          totalPrize = 0;
-          maxPrize = 10;
-          prize = 10;
-          maxRandom = 5;
-          i = -1;
-          numRandom = Math.floor(Math.random() * (maxRandom + 1));
-        } else {
-          break;
-        }
-      }
-    } else if (i == 2) {
-      game = confirm(`Thank you for a game. Your prize is: ${totalPrize}$\n` +
-                     `Do you want to play again?`);
-      if (game) {
-        totalPrize = 0;
-        maxPrize = 10;
-        prize = 10;
-        maxRandom = 5;
-        i = -1;
-        numRandom = Math.floor(Math.random() * (maxRandom + 1));
-      } else {
-        break;
-      }
-    } else if (i == 1) {
-      prize = Math.floor(prize / 2);
+      maxRandom *= 2;
+      maxPrize *= 3;
+      prize = maxPrize;
+      i = -1;
+      win = true;
+      numRandom = Math.floor(Math.random() * maxRandom + 1);
+      nextGame = confirm(`Do you want to continue game?`);
     } else {
-      prize = Math.floor(maxPrize / 2);
+      prize = Math.floor(maxPrize / (2 + i * 2));
+      win = false;
+    } 
+    if ( (i == 2 && !win) || !nextGame ) {
+      maxPrize = 10;
+      prize = maxPrize;
+      maxRandom = 5;
+      numRandom = Math.floor(Math.random() * maxRandom + 1);
+      nextGame = confirm(`Thank you for a game. Your prize is: ${totalPrize}$ \nDo you want to play again?`);
+      totalPrize = 0;
+      i = -1;
+    }
+    if (!nextGame) {
+      break;
     }
   }
 } else {
