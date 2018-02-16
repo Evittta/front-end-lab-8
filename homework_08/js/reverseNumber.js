5a9a80e246a3afb0f7603933446f7d0b12ca910b
Evita Kusinya <vita.kusinya@gmail.com>
2018-02-16 18:19:05 +0200

    delete brackets

--- homework_08/js/reverseNumber.js
+++ homework_08/js/reverseNumber.js
@@ -1,9 +1,9 @@
-let reverseNumber = (num) => {
+let reverseNumber = num => {
     let invert = num.toString().split('');
     if( num > 0) {
         invert = invert.reverse().join('');
     } else {
         invert = invert.slice(1).reverse().join('') * -1;
     }
-    return Number(invert);
+    return Number(invert); 
 }
