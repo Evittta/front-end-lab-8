4d256cb68bb26dfd4bb4ecb8cf5656a2ba9eae89
Evita Kusinya <vita.kusinya@gmail.com>
2018-02-17 00:43:43 +0200

    add function closestToZero

---
+++ homework_08/js/getClosestToZero.js
@@ -0,0 +1,7 @@
+function getClosestToZero() {
+  let closestToZero = arguments[0];
+  if ( Math.abs(arguments[i]) < Math.abs(closestToZero) ) {
+    closestToZero = arguments[i];
+  }
+  return closestToZero;
+}
