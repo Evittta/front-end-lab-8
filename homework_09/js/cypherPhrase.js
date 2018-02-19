d1d2d1e941f2a19240796dec93d003a09bec2247
Evita Kusinya <vita.kusinya@gmail.com>
2018-02-20 00:26:06 +0200

    add logical or

--- homework_09/js/cypherPhrase.js
+++ homework_09/js/cypherPhrase.js
@@ -1,12 +1,5 @@
 const cypherPhrase = (obj, str) => {
-    let res = str.split('');
-    res = getTransformedArray(res, item => {
-        for (let i in obj) {
-            if (item === i) {
-                item = obj[i];
-            }
-        }
-        return item;
-    });
-    return res.join('');
+    return getTransformedArray(str, item => {
+        return obj[item] || item;
+    }).join('');
 }
