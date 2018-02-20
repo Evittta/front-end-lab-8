b78280f8aa81c1e5c1e2ab774c9b109bef35f5b1
Evita Kusinya <vita.kusinya@gmail.com>
2018-02-20 21:10:20 +0200

    delete spaces

--- homework_09/js/decypherPhrase.js
+++ homework_09/js/decypherPhrase.js
@@ -1,7 +1,7 @@
 const decypherPhrase = (obj, str) => {
     const invertObj = {};
     for (let key in obj) {
-            invertObj[obj[key]] = key;
-        }
+        invertObj[obj[key]] = key;
+    }
     return cypherPhrase(invertObj, str);
 };
