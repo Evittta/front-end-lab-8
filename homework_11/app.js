710072029dc78278b6fcb5f3c4284891a8fd8e1f
Evita Kusinya <vita.kusinya@gmail.com>
2018-02-26 21:09:59 +0200

    rename variable of icons

--- homework_11/app.js
+++ homework_11/app.js
@@ -1,23 +1,22 @@
-let rootNode = document.getElementById("root");
-
+const rootNode = document.getElementById("root");
 function createTree(structure) {
   const ul = document.createElement("ul");
   for (let i = 0; i < structure.length; i++) {
     const li = document.createElement("li");
     const span = document.createElement("span");
-    const elemI = document.createElement("i");
-    elemI.className = "material-icons";
+    const icon = document.createElement("i");
+    icon.className = "material-icons";
     const div = document.createElement("div");
     li.appendChild(div);
     ul.appendChild(li);
-    div.appendChild(elemI);
+    div.appendChild(icon);
     div.appendChild(span);
     if (structure[i].folder) {
       div.className = "folder";
-      elemI.innerHTML = "folder";
+      icon.innerHTML = "folder";
       span.innerHTML = structure[i].title;
       if (structure[i].children) {
-        let childrenUl = createTree(structure[i].children);
+        const childrenUl = createTree(structure[i].children);
         li.appendChild(childrenUl);
       } else {
         const p = document.createElement("p");
@@ -27,9 +26,10 @@ function createTree(structure) {
       }
     } else {
       span.innerHTML = structure[i].title;
-      elemI.innerHTML = "insert_drive_file";
-      elemI.classList.add("file");
+      icon.innerHTML = "insert_drive_file";
+      icon.classList.add("file");
     }
+    div.addEventListener("click", openFolder);
   }
   return ul;
 }
