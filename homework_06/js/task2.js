f02cb22672b44f5e6072832fd13abe84677c20ea
Evita Kusinya <vita.kusinya@gmail.com>
2018-04-03 15:42:48 +0300

    rename variables

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -1,31 +1,53 @@
-const answer = document.querySelector(`.answer`);
+const answer = document.createElement(`p`);
+const communication = document.querySelector(`.communication`);
+const preloader = document.createElement(`div`);
+const loader = document.createElement(`div`);
+const main = document.getElementById(`main`);
+const responseInfo = document.createElement(`div`);
+const tableInfo = document.createElement(`table`);
+const tBody = document.createElement(`t-body`);
 let response;
+communication.appendChild(preloader);
+preloader.appendChild(loader);
+main.appendChild(responseInfo);
+responseInfo.appendChild(tableInfo);
+tableInfo.appendChild(tBody);
+preloader.appendChild(answer);
+answer.setAttribute(`class`, `answer`);
+preloader.setAttribute(`class`, `preloader`);
+loader.setAttribute(`class`, `loader`);
+responseInfo.setAttribute(`class`, `response-info`);
+tableInfo.setAttribute(`id`, `table`);
+tBody.setAttribute(`id`, `t-body`);
 
 const trackIP = () => {
   const ip = document.querySelector(`input`).value;
-  if (ip && validateIP(ip)) {
+  if (validateIP(ip)) {
     const url = `https://ipapi.co/${ip}/json`;
     http.get(url).then(getDetails, showError);
+    preloader.className = `preloader-active`;
+    loader.className = `loader-active`;
   } else {
     alert(`Please input correct IP address`);
   }
 };
+
 const getDetails = info => {
-  response = info;
   const data = JSON.parse(info);
   const validateResponseButton = document.querySelector(`.validate-button`);
-  const tableParent = document.querySelector(`.response-info`);
-  const tableInfo = document.getElementById(`table`);
   const oldTableBody = document.getElementById(`t-body`);
   tableInfo.removeChild(oldTableBody);
   const tableBody = tableInfo.createTBody();
   tableBody.setAttribute(`id`, `t-body`);
-  tableParent.appendChild(tableInfo);
+  responseInfo.appendChild(tableInfo);
+  response = info;
   for (const key in data) {
     const tableRow = tableBody.insertRow();
     createCell(tableRow, key);
     createCell(tableRow, data[key]);
   }
+  preloader.className = `preloader-end`;
+  loader.className = `loader-end`;
   validateResponseButton.style.display = `inline-block`;
   answer.style.display = `none`;
 };
@@ -37,11 +59,15 @@ const showError = statusCode => {
 const validateResponse = () => {
   const url = `https://shrouded-garden-94580.herokuapp.com/`;
   http.post(url, response).then(getAnswer, showError);
+  preloader.className = `preloader-active`;
+  loader.className = `loader-active`;
 };
 
 const getAnswer = data => {
   answer.innerHTML = data;
   answer.style.display = `block`;
+  preloader.className = `preloader-end`;
+  loader.className = `loader-end`;
 };
 
 const createCell = (parent, data) => {
@@ -51,12 +77,13 @@ const createCell = (parent, data) => {
   return cell;
 };
 
-document.getElementById(`track-ip`).addEventListener(`click`, trackIP);
-document
-  .querySelector(`.validate-button`)
-  .addEventListener(`click`, validateResponse);
 const validateIP = ip => {
   return /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
     ip
   );
 };
+
+document.getElementById(`track-ip`).addEventListener(`click`, trackIP);
+document
+  .querySelector(`.validate-button`)
+  .addEventListener(`click`, validateResponse);
