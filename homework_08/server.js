8e91d09da7562292d2951cbb5decb6f8e7259c9e
Evita Kusinya <vita.kusinya@gmail.com>
2018-04-10 16:16:53 +0300

    delete unnecessary code

--- homework_08/server.js
+++ homework_08/server.js
@@ -1,16 +1,11 @@
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
-const musiciansControllers = require(`./controllers/handlers`);
+const musicians = require(`./routes`);
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
-app.get(`/rockstars`, musiciansControllers.getMusicians);
-app.get(`/rockstar/:id`, musiciansControllers.getMusicianById);
-app.post(`/rockstar`, musiciansControllers.addNewMusician);
-app.put(`/rockstar/:id`, musiciansControllers.updateMusician);
-app.delete(`/rockstar/:id`, musiciansControllers.deleteMusician);
+app.use(`/`, musicians);
 
 process.stdout.write(`What are you waiting for? \n`);
 app.listen(port, () => console.log(`Listening on port ${port}...`));
